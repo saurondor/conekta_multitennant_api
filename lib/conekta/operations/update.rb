1910296f464e8340d09718287c160ebb974a7281
Gerardo Tasistro <gtasistro@tiempometa.com>
2019-11-07 08:31:12 -0600

    update order implemented

--- lib/conekta/operations/update.rb
+++ lib/conekta/operations/update.rb
@@ -1,8 +1,8 @@
 module Conekta
   module Operations
     module Update
-      def update(params)
-        response = Requestor.new.request(:put, self._url, params)
+      def update(api_key,params)
+        response = Requestor.new(api_key).request(:put, self._url, params)
         self.load_from(response)
         self
       end
